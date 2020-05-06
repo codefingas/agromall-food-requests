ac7b6118225bb49f0cab6b4746320a4e1534c3eb
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-05-06 14:28:19 +0100

    setting up cloud functions

---
+++ web/build/precache-manifest.17de7599e91dc8e55e6e28b123685a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb03242204a7095146361c02a043d37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963523f896c394edfbc2",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "2df3a2384523f7112951",
+    "url": "/static/css/main.881d93da.chunk.css"
+  },
+  {
+    "revision": "963523f896c394edfbc2",
+    "url": "/static/js/2.5c78a176.chunk.js"
+  },
+  {
+    "revision": "a320f89fa8bc0b00b69fe9c3f6d03da0",
+    "url": "/static/js/2.5c78a176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df3a2384523f7112951",
+    "url": "/static/js/main.ad2507de.chunk.js"
+  },
+  {
+    "revision": "93cd7b7c47fb3dd69402",
+    "url": "/static/js/runtime-main.99b37f35.js"
+  }
+]);
